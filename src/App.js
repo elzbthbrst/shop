06e5a2d599a197c391974e08e667975d2d10f4c5
yelyzaveta berest <lizaberest97@gmail.com>
2023-07-05 17:41:31 +0300

    created sections, added styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,62 @@
+import React from "react";
 
-function App() {
-  return (
-    <div>shop</div>)
+
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Items from "./components/Items";
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props) 
+    this.state = {
+      items: [
+        {
+          id: 1,
+          title: 'Стілець сірий',
+          img: 'chair-grey.jpeg',
+          desc: 'lorem ipsum dolor sit amet, consectetur adpacisang.',
+          category: 'chairs', 
+          price: '49.99'
+        },
+        {
+          id: 2,
+          title: 'Стіл',
+          img: 'table.web',
+          desc: 'lorem ipsum dolor sit amet, consectetur adpacisang.',
+          category: 'chairs', 
+          price: '49.99'
+        },
+        {
+          id: 3,
+          title: 'Стілець сірий',
+          img: 'chair-grey.jpeg',
+          desc: 'lorem ipsum dolor sit amet, consectetur adpacisang.',
+          category: 'chairs', 
+          price: '49.99'
+        },
+        {
+          id: 4,
+          title: 'Стілець сірий',
+          img: 'chair-grey.jpeg',
+          desc: 'lorem ipsum dolor sit amet, consectetur adpacisang.',
+          category: 'chairs', 
+          price: '49.99'
+        },
+      ]
+    }
+  }
+
+  render() {
+    return (
+    <div className="wrapper">
+      <Header/>
+      <Items items = {this.state.items}/>
+      <Footer/>
+    </div>
+    )
+  }
+  
 }
 
 export default App;
