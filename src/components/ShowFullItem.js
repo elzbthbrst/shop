ae424779b6a687f209294bb60e180c8a171fb277
yelyzaveta berest <lizaberest97@gmail.com>
2023-07-26 14:40:57 +0300

    initial liza commit

--- src/components/ShowFullItem.js
+++ src/components/ShowFullItem.js
@@ -34,6 +34,7 @@ export default function ShowFullItem({ onShowItem, item, onAdd }) {
                 <h2>{item.title}</h2>
                 <p>{item.desc}</p>
                 <b>{item.price}$</b>
+                
                 <div className='add-to-cart' onClick={onAddClick}>+</div>
             </div>
         </div>
